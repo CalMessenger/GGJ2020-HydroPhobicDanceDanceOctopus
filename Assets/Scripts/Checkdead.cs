ddb6daf20aa410eb7d75abd1515de5388da62551
dean16jackson <dean49jackson@yahoo.co.uk>
2020-02-02 13:47:02 +0000

    menu

---
+++ Assets/Scripts/Checkdead.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Checkdead : MonoBehaviour
+{
+    public GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((GameState.gameState == GameState.State.Dead) || (GameState.gameState == GameState.State.OutOfFuel))
+            {
+            button.SetActive(true);
+        }
+    }
+
+       public void gotomainmenu()
+        {
+            SceneManager.LoadScene("Menu");
+
+        }
+
+}
---
+++ Assets/nextscene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onclick()
+
+    {
+
+        SceneManager.LoadScene("MainScene_Jacob 2");
+    }
+
+    public void exit()
+
+    {
+
+        Application.Quit();
+    }
+}
